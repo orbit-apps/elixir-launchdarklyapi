a155fb8a5efecaa3cb370bdd0d0613a705a04f85
Hez Ronningen <code@hez.ca>
2020-11-26 16:00:02 -0800

    bump elixir ver and fix CI test setup

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LaunchDarklyAPI.MixProject do
     [
       app: :launch_darkly_api,
       version: "0.1.4",
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
