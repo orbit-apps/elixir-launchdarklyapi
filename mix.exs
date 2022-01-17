00f811ca0837cfa260111c6623c307a13a35b16f
Hez Ronningen <code@hez.ca>
2022-01-17 10:39:26 -0800

    remove unused dep

--- mix.exs
+++ mix.exs
@@ -30,7 +30,6 @@ defmodule LaunchDarklyAPI.MixProject do
       # Dev
       {:credo, "~> 1.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, ">= 0.0.0", only: [:dev, :test]},
       # Everything else
       {:hackney, "~> 1.17.0"},
       {:jason, "~> 1.0"},
